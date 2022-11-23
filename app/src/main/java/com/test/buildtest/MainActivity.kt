2941eaddbfdd9a57fccc10fe4c3a7bf82eb699a6
rencarLJW <jiwoolee@bemycar.co.kr>
2022-11-23 10:33:32 +0900

    commit

--- app/src/main/java/com/test/buildtest/MainActivity.kt
+++ app/src/main/java/com/test/buildtest/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.test.buildtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "VERSION_NAME : ${BuildConfig.VERSION_NAME}" +
+                "VERSION_CODE : ${BuildConfig.VERSION_CODE}" +
+                "is DEBUG : ${BuildConfig.DEBUG}"
     }
 }

650f01cc6084b5283993754f8fb26f4fe3cf9f5b
rencarLJW <jiwoolee@bemycar.co.kr>
2022-11-25 11:28:27 +0900

    build 140_6

--- app/src/main/java/com/test/buildtest/MainActivity.kt
+++ app/src/main/java/com/test/buildtest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+//
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
